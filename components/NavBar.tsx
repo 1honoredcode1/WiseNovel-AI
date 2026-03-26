4d2577095163a80f8251f0fe5208898472586545
HonoredCodePrince <klevis25sfj@gmail.com>
2026-03-26 19:04:26 +0100

    Navbar implemented

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
 
 const NavBar = () => {
+  const navItems = [
+    { label: "Library", href: "/" },
+    { label: "Add New", href: "/books/new" },
+  ];
+
+  const pathname = usePathname();
+
   return (
     <header className="w-full fixed z-50 bg-('--bg-primary') ">
       <div className="wrapper navbar-height py-4 flex justify-between items-center">
@@ -12,7 +24,26 @@ const NavBar = () => {
             width={42}
             height={26}
           />
+          <span className="logo-text">WiseNovel AI</span>
         </Link>
+        <nav className="w-fit flex gap-7.5 items-center">
+          {navItems.map(({ label, href }) => {
+            const isActive =
+              pathname === href || (href !== "/" && pathname.startsWith(href));
+            return (
+              <Link
+                key={label}
+                href={href}
+                className={cn(
+                  "nav-link-base",
+                  isActive ? "nav-link-active" : "text-black hover:opacity-70",
+                )}
+              >
+                {label}{" "}
+              </Link>
+            );
+          })}
+        </nav>
       </div>
     </header>
   );
